02889bd024b1860335f89f0023251f4cc9167d86
sirvin01 <91463820+sirvin01@users.noreply.github.com>
2022-08-22 11:16:56 +0300

    u

---
+++ base/api/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import field
+from pyexpat import model
+from rest_framework.serializers import ModelSerializer
+from base.models import Room
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
