cfe1cc17946116b8c04d426ce386cf8b6356c8c9
sirvin01 <91463820+sirvin01@users.noreply.github.com>
2022-10-03 12:10:10 +0300

    new

--- base/views.py
+++ base/views.py
@@ -68,11 +68,11 @@ def home(request):
     )
 
     topics = Topic.objects.all()
-    room_count =  rooms.count()
+    # room_count =  rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__contains=q))
 
     context = {'rooms':rooms, 'topics':topics,
-    'room_count':room_count,'room_messages':room_messages}
+    'room_messages':room_messages}
 
     return render(request,'base/home.html',context)
 
